2ac3fcc8cb3790eb50e517b9703e46485571a32e
Mark Pavlidis <mark@pavlidis.ca>
2013-01-06 20:42:16 -0500

    Redacted Faces Demo
    An app to demonstrate how CIDetector finds faces then anonymizes them by applying the CIPixellate filter
    
    Signed-off-by: Mark Pavlidis <mark@pavlidis.ca>

--- RedactedFaces/MHPRFViewController.m
+++ RedactedFaces/MHPRFViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "MHPRFViewController.h"
+#import <CoreImage/CoreImage.h>
 
 @interface MHPRFViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) CIContext *context;
+
+- (IBAction)tappedOriginal:(id)sender;
+- (IBAction)tappedRedacted:(id)sender;
+
 
 @end
 
@@ -17,13 +25,65 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.imageView.image = [UIImage imageNamed:@"faces"];
+    self.context = [CIContext contextWithOptions:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)tappedOriginal:(id)sender {
+    self.imageView.image = [UIImage imageNamed:@"faces"];
+}
+
+- (IBAction)tappedRedacted:(id)sender {
+    CIImage *image = [CIImage imageWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"faces" withExtension:@"png"]];
+    
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
+                                              context:nil
+                                              options:nil];
+    NSArray *faces = [detector featuresInImage:image options:nil];
+    
+    // Create a green circle to cover the rects that are returned.    
+    CIImage *maskImage = nil;
+    
+    for (CIFeature *face in faces) {
+        CIVector *centre = [CIVector vectorWithX:face.bounds.origin.x + face.bounds.size.width/2.0
+                                               Y:face.bounds.origin.y + face.bounds.size.height/2.0];
+        CGFloat radius = MIN([face bounds].size.width, [face bounds].size.height)/1.5;
+        CIFilter *radialGradient = [CIFilter filterWithName:@"CIRadialGradient" keysAndValues:
+                                    @"inputRadius0", @(radius),
+                                    @"inputRadius1", @(radius + 1.0),
+                                    @"inputColor0", [CIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0], // Green
+                                    @"inputColor1", [CIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0], // Transparent
+                                    @"inputCenter", centre,
+                                    nil];
+        CIImage *circleImage = radialGradient.outputImage;
+
+        if (nil == maskImage)
+            maskImage = circleImage;
+        else
+            maskImage = [[CIFilter filterWithName:@"CISourceOverCompositing" keysAndValues:
+                          @"inputImage", circleImage,
+                          @"inputBackgroundImage", maskImage,
+                          nil]
+                         outputImage];        
+    }
+    
+    CIImage *pixellatedImage = [[CIFilter filterWithName:@"CIPixellate" keysAndValues:
+                                 kCIInputImageKey, image,
+                                 @"inputScale", @(32),
+                                 nil]
+                               outputImage];
+    
+    CIImage *redactedImage = [[CIFilter filterWithName:@"CIBlendWithMask" keysAndValues:
+                               kCIInputImageKey, pixellatedImage,
+                               kCIInputBackgroundImageKey, image,
+                               @"inputMaskImage", maskImage,
+                               nil]
+                              outputImage];
+    
+    
+    CGImageRef cgImage = [self.context createCGImage:redactedImage fromRect:redactedImage.extent];
+    self.imageView.image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
 }
 
 @end
