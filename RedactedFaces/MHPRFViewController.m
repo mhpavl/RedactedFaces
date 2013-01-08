639d027ec95f02d5ded1b6b277e38d8885033de1
Mark Pavlidis <mark@pavlidis.ca>
2013-01-08 02:08:50 -0500

    Added button to show the mask image
    Signed-off-by: Mark Pavlidis <mark@pavlidis.ca>

--- RedactedFaces/MHPRFViewController.m
+++ RedactedFaces/MHPRFViewController.m
@@ -13,9 +13,12 @@
 
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) CIContext *context;
+@property (strong, nonatomic) UIImage *redactedImage;
+@property (strong, nonatomic) UIImage *mask;
 
 - (IBAction)tappedOriginal:(id)sender;
 - (IBAction)tappedRedacted:(id)sender;
+- (IBAction)tappedMask:(id)sender;
 
 
 @end
@@ -34,29 +37,62 @@
 }
 
 - (IBAction)tappedRedacted:(id)sender {
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.imageView.alpha = 0.2;
+                     } completion:^(BOOL finished) {
+                         [self redactImage];
+                         self.imageView.image = self.redactedImage;
+                         [UIView animateWithDuration:0.25
+                                          animations:^{
+                                              self.imageView.alpha = 1;
+                                          }];
+                     }];
+}
+
+- (IBAction)tappedMask:(id)sender {
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.imageView.alpha = 0.2;
+                     } completion:^(BOOL finished) {
+                         [self redactImage];
+                         self.imageView.image = self.mask;
+                         [UIView animateWithDuration:0.25
+                                          animations:^{
+                                              self.imageView.alpha = 1;
+                                          }];
+
+                     }];
+}
+
+- (void)redactImage
+{
+    if (self.redactedImage != nil)
+        return;
+    
     CIImage *image = [CIImage imageWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"faces" withExtension:@"png"]];
     
     CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
-                                              context:nil
+                                              context:self.context
                                               options:nil];
     NSArray *faces = [detector featuresInImage:image options:nil];
     
-    // Create a green circle to cover the rects that are returned.    
+    // Create a green circle to cover the rects that are returned.
     CIImage *maskImage = nil;
     
     for (CIFeature *face in faces) {
         CIVector *centre = [CIVector vectorWithX:face.bounds.origin.x + face.bounds.size.width/2.0
                                                Y:face.bounds.origin.y + face.bounds.size.height/2.0];
-        CGFloat radius = MIN([face bounds].size.width, [face bounds].size.height)/1.5;
+        CGFloat radius = MIN([face bounds].size.width, [face bounds].size.height)/2.0;
         CIFilter *radialGradient = [CIFilter filterWithName:@"CIRadialGradient" keysAndValues:
                                     @"inputRadius0", @(radius),
-                                    @"inputRadius1", @(radius + 1.0),
+                                    @"inputRadius1", @(radius * 1.5),
                                     @"inputColor0", [CIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0], // Green
                                     @"inputColor1", [CIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0], // Transparent
                                     @"inputCenter", centre,
                                     nil];
         CIImage *circleImage = radialGradient.outputImage;
-
+        
         if (nil == maskImage)
             maskImage = circleImage;
         else
@@ -64,14 +100,14 @@
                           @"inputImage", circleImage,
                           @"inputBackgroundImage", maskImage,
                           nil]
-                         outputImage];        
+                         outputImage];
     }
     
     CIImage *pixellatedImage = [[CIFilter filterWithName:@"CIPixellate" keysAndValues:
                                  kCIInputImageKey, image,
-                                 @"inputScale", @(32),
+                                 @"inputScale", @(16),
                                  nil]
-                               outputImage];
+                                outputImage];
     
     CIImage *redactedImage = [[CIFilter filterWithName:@"CIBlendWithMask" keysAndValues:
                                kCIInputImageKey, pixellatedImage,
@@ -80,10 +116,11 @@
                                nil]
                               outputImage];
     
+    self.mask = [UIImage imageWithCIImage:maskImage];
     
     CGImageRef cgImage = [self.context createCGImage:redactedImage fromRect:redactedImage.extent];
-    self.imageView.image = [UIImage imageWithCGImage:cgImage];
-    CGImageRelease(cgImage);
+    self.redactedImage = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);    
 }
 
 @end
